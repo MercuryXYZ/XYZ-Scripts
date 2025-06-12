00200477771e20aca1d0b8b96ab479c4c05fdade
MercuryXYZ <mercuryxyzbusinessmail@gmail.com>
2025-06-12 21:48:56 +0200

    main

--- loader.lua
+++ loader.lua
@@ -13,7 +13,7 @@ local games = {
 local placeId = game.PlaceId
 local currentGame = games[placeId]
 
-local loadingScreen = loadstring(game:HttpGet("", true))()
+local loadingScreen = loadstring(game:HttpGet("https://raw.githubusercontent.com/MercuryXYZ/XYZ-Scripts/refs/heads/main/loadingscreen.lua", true))()
 
 if currentGame and currentGame.script then
     loadingScreen:Start(currentGame.name or "Unknown Game", currentGame.script)
